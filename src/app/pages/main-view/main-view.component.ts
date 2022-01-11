8270235ffcc996a0c6a5a3f2ded094110de26704
Michał Płużek <michalpluzek@gmail.com>
2022-01-11 15:04:09 +0100

    feat: Add dynamic create board and columns

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -4,6 +4,8 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
 
 @Component({
   selector: 'app-main-view',
@@ -11,9 +13,27 @@ import {
   styleUrls: ['./main-view.component.scss'],
 })
 export class MainViewComponent {
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  board: Board = new Board('Test Board', [
+    new Column('Ideas', [
+      'Some ideas',
+      'This is very intresting',
+      "I'm lovin it",
+    ]),
+    new Column('Research', ['Lorem ipsum', 'Research column']),
+    new Column('Todo', [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep',
+    ]),
+    new Column('Done', [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog',
+    ]),
+  ]);
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
